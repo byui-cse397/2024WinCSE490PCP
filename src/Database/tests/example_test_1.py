92727df9d4732065997c41e5932d3572fa6b2dd6
173brian <173brian@gmail.com>
2024-02-28 22:54:57 -0700

    Setup DB test methods format

---
+++ src/Database/tests/example_test_1.py
@@ -0,0 +1,17 @@
+from mysql.connector import MySQLConnection, Error
+from db_test import DBTest
+
+
+class ExampleTest1(DBTest):
+    def _test(self, connection: MySQLConnection) -> bool:
+        """
+        This is an example of syntax that we can use to access the DB
+        """
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT 1 FROM Account LIMIT 1")
+            # Yay! Data exists!
+            return True
+        except Error:
+            # Oh no! Something went wrong!
+            return False
