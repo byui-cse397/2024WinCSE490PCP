be03a49e4122c7a68625a85b6105d5f36b8ef524
173brian <173brian@gmail.com>
2024-02-28 22:01:24 -0700

    Updated DB init script.

--- src/Database/init.sql
+++ src/Database/init.sql
@@ -1,3 +1,5 @@
+---Database Connection Setup---
+
 CREATE DATABASE IF NOT EXISTS linkup_db;
 
 USE linkup_db;
@@ -8,3 +10,12 @@ CREATE USER IF NOT EXISTS 'linkup-user'@'localhost' IDENTIFIED BY 'user';
 GRANT SELECT, INSERT, UPDATE, DELETE ON linkup_db.* TO 'linkup-user'@'localhost';
 
 FLUSH PRIVILEGES;
+
+---Table Schema Setup---
+
+CREATE TABLE IF NOT EXISTS Account (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
