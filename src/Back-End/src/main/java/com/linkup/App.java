2e6379b970a0583b5e2be336b051fd4145b2ed87
Allan <allanmarina33@gmail.com>
2024-03-21 01:17:26 -0700

    Updated App file with comments

--- src/Back-End/src/main/java/com/linkup/App.java
+++ src/Back-End/src/main/java/com/linkup/App.java
@@ -4,36 +4,55 @@ import com.linkup.database.exceptions.FrontEndUsageException;
 import com.linkup.user.UserManager;
 import java.util.logging.*;
 
+/**
+ * Main class of the community board mobile application backend.
+ * Responsible for initializing components and managing database connections.
+ */
 public class App {
+  // Logger instance for logging application events
   static Logger logger;
+
+  /**
+   * Main method to start the application.
+   * @param args Command-line arguments.
+   */
   public static void main(String[] args) {
     // Setup logging:
     logger = LoggingManager.getLogger();
     // Resolve database host:
     establishConnection();
 
+    // Create a new user:
     UserManager manager = new UserManager();
     try {
+      // Attempt to create a new user and retrieve the user ID
       Integer userID = manager.createNewUser("username_002", "email@domain.net",
                                              "password_002");
+      // Log successful user creation and login
       StringBuilder sb = new StringBuilder();
       sb.append("User ").append(userID).append(
           " created and logged in successfully.");
       System.out.println(sb.toString());
     } catch (FrontEndUsageException e) {
-      // parse this message and send it to FE
+      // Handle frontend usage exception by parsing the message and sending it to the frontend
     }
 
     // Stop database server and close connection:
     closeConnection();
   }
 
+  /**
+   * Establishes connection with the database server.
+   */
   private static void establishConnection() {
     ConnectionManager.getInstance("https://localhost:4039");
     // System.out.println("Connection established with Database.");
     logger.info("Connection established with Database.");
   }
 
+  /**
+   * Closes the connection with the database server.
+   */
   private static void closeConnection() {
     ConnectionManager instance = ConnectionManager.getInstance(null);
     HTTPSConnector connection = instance.getConnector();
