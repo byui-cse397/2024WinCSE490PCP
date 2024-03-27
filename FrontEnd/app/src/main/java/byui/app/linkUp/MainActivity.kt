66be85c63b7819d73318c75cfab46f65774066fc
RoguePiranha <akswayze@gmail.com>
2024-03-27 14:02:33 -0600

    Updated colors and some strings

--- FrontEnd/app/src/main/java/byui/app/linkUp/MainActivity.kt
+++ FrontEnd/app/src/main/java/byui/app/linkUp/MainActivity.kt
@@ -1,5 +1,5 @@
 package byui.app.linkUp
-//
+
 //import android.os.Bundle
 //import com.google.android.material.bottomnavigation.BottomNavigationView
 //import androidx.appcompat.app.AppCompatActivity
