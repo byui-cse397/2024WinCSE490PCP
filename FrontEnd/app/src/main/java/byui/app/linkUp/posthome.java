e5bfa171bc5ea1a32013ae40725d076c10298c94
Darcy Merilan <91292053+Darcymer@users.noreply.github.com>
2024-03-27 22:17:49 -0400

    Update posthome.java

--- FrontEnd/app/src/main/java/byui/app/linkUp/posthome.java
+++ FrontEnd/app/src/main/java/byui/app/linkUp/posthome.java
@@ -1,7 +1,56 @@
 package byui.app.linkUp;
 
 
-public class posthome {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-    
+public class posthome extends AppCompatActivity {
+
+    Button toCreatePostButton, toAccountPageButton, toLogoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.posthome);
+
+        
+        toCreatePostButton = findViewById(R.id.tocreatepost);
+        toAccountPageButton = findViewById(R.id.toaccountpage);
+        toLogoutButton = findViewById(R.id.tologout);
+
+       
+        toCreatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start create post activity
+                Intent intent = new Intent(posthome.this, createpost.class);
+                startActivity(intent);
+            }
+        });
+
+        
+        toAccountPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start account page activity
+                Intent intent = new Intent(posthome.this, Accountpage.class);
+                startActivity(intent);
+            }
+        });
+
+        
+        toLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start login activity
+                Intent intent = new Intent(posthome.this, Login.class);
+                startActivity(intent);
+                // Finish current activity
+                finish();
+            }
+        });
+    }
 }
