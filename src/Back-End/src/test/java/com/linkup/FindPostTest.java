3d2bb63931eedfbaf82230167e98d56b2960f682
Allan <allanmarina33@gmail.com>
2024-03-29 12:42:06 -0700

    update all post tests

--- src/Back-End/src/test/java/com/linkup/FindPostTest.java
+++ src/Back-End/src/test/java/com/linkup/FindPostTest.java
@@ -1,4 +1,41 @@
 package com.linkup;
 
+import static org.junit.Assert.*;
+
+import com.linkup.database.exceptions.FrontEndUsageException;
+import com.linkup.posts.FindPost;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
 public class FindPostTest {
+    @Test
+    public void testFindPostByAccount() {
+        // Arrange
+        FindPost findPost = new FindPost(FindPost.FILTER.ACCOUNTS, 123);
+
+        // Act
+        String query = findPost.buildQuery(createColValueMap(findPost));
+
+        // Assert
+        assertEquals("SELECT * FROM POST WHERE accounts_id = 123;", query);
+    }
+
+    @Test
+    public void testFindPostByCommunity() {
+        // Arrange
+        FindPost findPost = new FindPost(FindPost.FILTER.COMMUNITY, 456);
+
+        // Act
+        String query = findPost.buildQuery(createColValueMap(findPost));
+
+        // Assert
+        assertEquals("SELECT * FROM POST WHERE community_id = 456;", query);
+    }
+
+    private Map<String, String> createColValueMap(FindPost findPost) {
+        Map<String, String> colValueMap = new HashMap<>();
+        colValueMap.put("operation", findPost.operation);
+        colValueMap.put("filterValue", String.valueOf(findPost.filterValue));
+        return colValueMap;
+    }
 }
--- src/Back-End/src/test/java/com/linkup/UpdatePostTest.java
+++ src/Back-End/src/test/java/com/linkup/UpdatePostTest.java
@@ -1,4 +1,74 @@
 package com.linkup;
 
+import static org.junit.Assert.*;
+
+import com.linkup.database.exceptions.FrontEndUsageException;
+import com.linkup.posts.UpdatePost;
+import org.junit.Test;
 public class UpdatePostTest {
+    @Test
+    public void testValidPostUpdate() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "Updated post text", 456, 789);
+
+        // Act
+        boolean result = updatePost.checks();
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testNullPostText() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, null, 456, 789);
+
+        // Act
+        updatePost.checks();
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testEmptyPostText() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "", 456, 789);
+
+        // Act
+        updatePost.checks();
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testNullAccountId() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "Updated post text", null, 789);
+
+        // Act
+        updatePost.checks();
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testInvalidAccountId() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "Updated post text", 0, 789);
+
+        // Act
+        updatePost.checks();
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testNullCommunityId() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "Updated post text", 456, null);
+
+        // Act
+        updatePost.checks();
+    }
+
+    @Test(expected = FrontEndUsageException.class)
+    public void testInvalidCommunityId() throws FrontEndUsageException {
+        // Arrange
+        UpdatePost updatePost = new UpdatePost(123, "Updated post text", 456, 0);
+
+        // Act
+        updatePost.checks();
+    }
 }
