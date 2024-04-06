c5b9aa5e2f89f671693ac33e788d2d72c43f61e8
bananablizzard <55606174+joshuasbee@users.noreply.github.com>
2024-04-06 17:29:49 -0400

    Added useful scripts in comments of init.sql

--- src/Database/init.sql
+++ src/Database/init.sql
@@ -81,3 +81,48 @@ CREATE TABLE IF NOT EXISTS MESSAGES (
     receiver_id INT NOT NULL
     -- add foreign key for sender and receiver id's
 );
+
+-- Helpful Scripts
+/* 
+-- You can copy this to use for running the test cases to initialize the DB --
+use linkup_db; 
+
+insert into ACCOUNT (username, password_hash)
+VALUES ('hello', 'alks;dfjashg8');
+
+insert into community (community_name, parent_account_id)
+VALUES ('Rexburg', 1);
+
+insert into POST (content_text, account_id, community_id, post_time)
+VALUES ('This is a post', 1, 1, current_timestamp());
+*/
+
+/*
+-- This shows you how many items are in each table of the DB, useful for testing
+SELECT
+  (SELECT COUNT(*) FROM account) AS account_count,
+  (SELECT COUNT(*) FROM community) AS community_count,
+  (SELECT COUNT(*) FROM post) AS post_count,
+  (SELECT COUNT(*) FROM messages) AS messages_count,
+  (SELECT COUNT(*) FROM departing) AS departing_count,
+  (SELECT COUNT(*) FROM arriving) AS arriving_count,
+  (SELECT COUNT(*) FROM forums) AS forums_count,
+  (SELECT COUNT(*) FROM comments) AS comments_count
+;
+
+/*
+-- This will drop all ROWS in the DB, but not the schema (so you can insert right back into it if you need to, but ID's might not be right.)
+USE linkup_db;
+SET foreign_key_checks = 0;
+
+TRUNCATE MESSAGES; 
+TRUNCATE DEPARTING;
+TRUNCATE ARRIVING;
+TRUNCATE FORUMS;
+TRUNCATE COMMENTS;
+TRUNCATE POST; 
+TRUNCATE COMMUNITY;
+TRUNCATE ACCOUNT;
+
+SET foreign_key_checks = 1;
+*/
